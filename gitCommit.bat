3362cad8dd240e393e12a8fde26303459696b5a7
bella <yoonjeon0413@gmail.com>
2020-06-08 12:08:44 +0900

    bat test5

---
+++ gitCommit.bat
@@ -0,0 +1,24 @@
+cd public
+
+git add .
+
+@echo off
+
+echo Commit on GitHub
+echo.
+
+set /p comment="Enter comment: "
+git add .
+git commit -m "%comment%"
+git push origin master
+
+cd ..
+git add .
+set /p comment="Enter comment: "
+git commit -m "%comment%"
+git push origin master
+
+echo.
+echo All done :)
+
+pause
---
+++ test.bat
@@ -0,0 +1,6 @@
+@echo on
+
+echo 안녕하세요
+echo hahaha...
+
+cmd.exe
